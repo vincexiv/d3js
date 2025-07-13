cd82f206150d07feec91c2745fe7751a85b42d55
Vincent <makokhavomondi@gmail.com>
2025-07-13 16:36:15 +0300

    6.4.1 Dendogram

--- ch6/index.js
+++ ch6/index.js
@@ -1,5 +1,6 @@
-d3.csv("parents.csv").then(data => handleParents(data))
-d3.json("tweets.json").then(data => handleTweets(data))
+// d3.csv("parents.csv").then(data => handleParents(data))
+// d3.json("tweets.json").then(data => packedCircles(data))
+d3.json("tweets.json").then(data => dendogram(data))
 
 function handleParents(data){
     const stratify = d3.stratify()
@@ -9,7 +10,67 @@ function handleParents(data){
     const strata = stratify(data)
 }
 
-function handleTweets(data){
+function dendogram(data){
+    const depthScale = d3.scaleOrdinal().range(["#5EAFC6", "#FE9922", "#93c464", "#75739F"])
+
+    const groups = d3.group(data.tweets, d => d.user)
+    const hierarchy = d3.hierarchy(groups, d => d[1])
+        .sum(d => {
+            const favorites = d.favorites?.length || 0
+            const retweets = d.retweets?.length || 0
+            return favorites + retweets + 1
+        })
+
+    const tree = d3.tree()
+        .size([400, 400])
+
+    const treeChart = tree(hierarchy)
+
+    d3.select("svg")
+        .selectAll("line.link")
+        .data(treeChart.descendants().filter(d => d.parent))
+        .enter()
+        .append("line")
+        .attr("class", "link")
+        .attr("y1", d => d.y)    
+        .attr("x1", d => d.x)
+        .attr("x2", d => d.parent.x)
+        .attr("y2", d => d.parent.y)
+        .attr("transform", "translate(50, 50)")
+        .style("stroke", "black")
+
+    d3.select("svg")
+        .append("g")
+        .attr("transform", "translate(50, 50)")
+        .selectAll("g.node")
+        .data(treeChart.descendants())
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", d => `translate(${d.x}, ${d.y})`)
+
+    d3.selectAll("g.node")
+        .append("circle")
+        .attr("r", 10)
+        .style("fill", d => depthScale(d.depth))   
+        
+    d3.selectAll("g.node")
+        .append("text")
+        .style("text-anchor", "middle")
+        .style("fill", "#4f442b")
+        .text(d => {
+            if(d.depth == 0){
+                return 'All tweets'
+            } else if (d.depth == 1){
+                return d.data[0]
+            } else if (d.depth == 2){
+                return d.data.content
+            }
+        })
+
+}
+
+function packedCircles(data){
     const groups = d3.group(data.tweets, d => d.user)
 
     const depthScale = d3.scaleOrdinal().range(["#5EAFC6", "#FE9922", "#93c464", "#75739F"])
