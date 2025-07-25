ccc3f48955ee8f1e34b56db90887d3d610d6b267
Vincent <makokhavomondi@gmail.com>
2025-07-25 22:29:47 +0300

    8.1.2 Drawing points on a map

--- ch8/index.js
+++ ch8/index.js
@@ -1,8 +1,29 @@
-d3.json("world.geojson").then(countries => {
-    createMap(countries)
-})
+function world(){
+    d3.json("world.geojson").then(world => {
+        createMap(world)
+    })
+}
 
-function createMap(countries){
+function cities(){
+    const PromiseWrapper = (xhr, d) => {
+        return new Promise(resolve => {
+            xhr(d).then(d => resolve(d))
+        })
+    }
+    const a = PromiseWrapper(d3.json, "world.geojson")
+    const b = PromiseWrapper(d3.csv, "cities.csv")
+    const allData = Promise.all([a, b])
+    
+    allData.then(resolve => {
+        const world = resolve[0]
+        const cities = resolve[1]
+    
+        createMap(world)
+        markCities(cities)
+    })
+}
+
+function createMap(world){
     const projection = d3.geoMercator()
         .scale(80)
         .translate([250, 250])
@@ -12,7 +33,7 @@ function createMap(countries){
         .append("g")
         .attr("id", "world")
         .selectAll("path.country")
-        .data(countries.features)
+        .data(world.features)
         .enter()
         .append("path")
         .attr("class", "country")
@@ -20,3 +41,27 @@ function createMap(countries){
         .attr("id", d => d.id)
         .style("fill", "darkgrey")
 }
+
+function markCities(cities){
+    const projection = d3.geoMercator()
+        .scale(80)
+        .translate([250, 250])
+
+    d3.select("svg")
+        .append("g")
+        .attr("id", "cities")
+        .selectAll("circle")
+        .data(cities)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("cx", d => projection([d.x, d.y])[0])
+        .attr("cy", d => projection([d.x, d.y])[1])
+        .style("fill", "yellow")
+        .style("stroke", "black")
+        .style("opacity", 0.8)
+}
+
+
+
+cities()
